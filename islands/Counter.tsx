9c7893e49e98840ef073766a13e328621a885d6f
fry69 <142489379+fry69@users.noreply.github.com>
2025-09-02 15:56:56 +0200

    feat: testing <Head> component

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,22 +1,26 @@
-import type { Signal } from "@preact/signals";
+import { useSignal, type Signal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
 import { useEffect } from "preact/hooks";
+import { Head } from "fresh/runtime";
 
 interface CounterProps {
   count: Signal<number>;
 }
 
 export default function Counter({ count }: CounterProps) {
+  const newTitle = useSignal("Fresh Counter");
   useEffect(() => {
     // make sure we always stay Fresh, even if updated elsewhere
     if (count.value < 1) count.value = 1;
-
-    document.title = count.value + " Fresh Counter" +
+    newTitle.value = count.value + " Fresh Counter" +
       (Math.abs(count.value) === 1 ? "" : "s");
   }, [count.value]);
 
   return (
     <div class="flex gap-8 py-6">
+      <Head>
+        <title>{newTitle}</title>
+      </Head>
       <Button
         id="decrement"
         onClick={() => count.value = Math.max(1, count.value - 1)}
